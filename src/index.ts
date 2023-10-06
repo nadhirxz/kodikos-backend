717d794c7af10dba3b9209a5c6c8ce1a91f45b9f
Nadhir Ferdji <nadhirxz@gmail.com>
2023-10-06 11:36:13 +0100

    Add router

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import dotenv from 'dotenv';
+import router from './router';
 
 dotenv.config();
 
 const app = express();
 const port = parseInt(process.env.PORT) ?? 4000;
 
-app.get('/', (req: Request, res: Response) => {
-	res.send('API');
-});
+app.use(express.json());
+app.use('/', router);
 
 const server = app.listen(port, () => {
 	console.log(`Server is running at http://localhost:${port}`);
