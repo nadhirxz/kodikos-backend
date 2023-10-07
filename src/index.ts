19f4e81f4d90936b7e88de84efacd8629177e7e2
Nadhir Ferdji <nadhirxz@gmail.com>
2023-10-07 08:49:52 +0100

    Add cors

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import router from './router';
+import { verifyToken } from './utils/jwt';
+import db from './utils/db';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
-const port = parseInt(process.env.PORT) ?? 4000;
+const port = parseInt(process.env.PORT!) ?? 4000;
 
 app.use(express.json());
+app.use(cors({ origin: '*' }));
 app.use('/', router);
 
 const server = app.listen(port, () => {
