6a752c272e0b623abae112830eb5c08643a816a0
Nadhir Ferdji <nadhirxz@gmail.com>
2023-11-17 20:49:56 +0100

    Add env.d.ts

---
+++ env.d.ts
@@ -0,0 +1,15 @@
+declare global {
+	namespace NodeJS {
+		interface ProcessEnv {
+			readonly NODE_ENV: 'development' | 'production' | 'test';
+			readonly PORT: string;
+			readonly DATABASE_URL: string;
+			readonly FRONTEND_URL: string;
+			readonly HASH_SECRET_SALT: string;
+			readonly STRIPE_SECRET_KEY: string;
+			readonly ENDPOINT_SECRET: string;
+		}
+	}
+}
+
+export {};
