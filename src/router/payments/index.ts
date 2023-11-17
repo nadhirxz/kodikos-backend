66fc87fac91ab84c98a3e0cbb26890edd043f9de
Nadhir Ferdji <nadhirxz@gmail.com>
2023-11-17 20:49:43 +0100

    Add stripe webhook endpoint

--- src/router/payments/index.ts
+++ src/router/payments/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import createLink from './create-link';
+import webhook from './webhook';
 import { auth } from '../../utils/routeValidation';
 
 const router = Router();
 
-router.use('/create-link', auth, createLink);
+router.use('/create-link', auth, webhook);
+router.use('/webhook', auth, createLink);
 
 export default router;
---
+++ src/router/payments/webhook.ts
@@ -0,0 +1,63 @@
+import { Router } from 'express';
+import stripe, { Stripe } from 'stripe';
+import db from '../../utils/db';
+
+const router = Router();
+
+router.post('/', async (req, res) => {
+	const payload = req.body;
+	const sig = req.headers['stripe-signature'];
+
+	if (!sig) return res.status(400).send('No signature');
+
+	let event: stripe.Event;
+
+	try {
+		event = stripe.webhooks.constructEvent(payload, sig, process.env.ENDPOINT_SECRET!);
+	} catch (err) {
+		return res.status(400).send(`Webhook Error: ${err.message}`);
+	}
+
+	const session = event.data.object as Stripe.Checkout.Session;
+
+	const userId = session.metadata!.userId as string;
+	const processing = session.metadata!.processing as unknown as boolean;
+
+	const listing = await db.listing.findUnique({
+		where: { id: session.metadata!.listingId as string },
+		include: {
+			data: true,
+		},
+	});
+
+	if (!listing) return res.status(400).json({ error: 'Listing does not exist' });
+
+	await db.$transaction(async tx => {
+		await tx.data.update({
+			where: { id: listing.data.id },
+			data: { owner: { connect: { id: userId } } },
+		});
+
+		await tx.listing.delete({ where: { id: listing.id } });
+
+		await tx.transaction.create({
+			data: {
+				seller: { connect: { id: listing.data.ownerId } },
+				buyer: { connect: { id: userId } },
+				price: listing.price + (processing ? 0 : listing.processingPrice),
+				extraServices: processing
+					? undefined
+					: {
+							create: {
+								type: 'processing',
+								price: listing.processingPrice,
+							},
+					  },
+			},
+		});
+	});
+
+	res.status(200).end();
+});
+
+export default router;
