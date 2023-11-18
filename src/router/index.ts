deec2c7f05e967d66c53636b9d6bec3c5e45693d
Nadhir Ferdji <nadhirxz@gmail.com>
2023-11-18 11:46:43 +0100

    Add routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express';
 import loginRouter from './login';
 import registerRouter from './register';
+import paymentsRouter from './payments';
+import dataRouter from './data';
 import { noAuth } from '../utils/routeValidation';
 
 const router = Router();
 
 router.use('/register', noAuth, registerRouter);
 router.use('/login', noAuth, loginRouter);
+router.get('/payments', paymentsRouter);
+router.get('/data', dataRouter);
 router.get('/', (req, res) => {
 	res.send('API');
 });
